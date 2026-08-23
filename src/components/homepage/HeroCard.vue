d5b768b84232cd048945190a4f030a9bb1b73229
daniellife624 <cytcytcytcyt168@gmail.com>
2026-08-24 00:01:16 +0800

    feat: 新增 iOS 主畫面 icon（幾何 D 字標）、首頁定位文字調整
    - 新增 apple-touch-icon.png（180x180，黃底 + 粗線條幾何 D），index.html
      加上 apple-touch-icon 連結與 apple-mobile-web-app-title
    - 瀏覽器分頁 favicon 也換成同一個 D 字標（原本是文字排版的 D，樣式不一致）
    - HeroCard.vue 定位文字改為「NTU ACCT ／ Information Business Consultant
      ／ UI/UX Design」，聚焦會計背景 + 資訊顧問方向 + UI/UX 興趣

--- src/components/homepage/HeroCard.vue
+++ src/components/homepage/HeroCard.vue
@@ -8,7 +8,7 @@
           <h1 class="hero__name">周彥廷 <span class="hero__age">({{ age }})</span></h1>
           <p class="hero__meta">Taipei ／ 臺中海線人 ／ Male ／ <span class="hero__traits">沉穩、負責任、有企圖心</span></p>
         </div>
-        <p class="hero__subtitle">NTU ACCT ／ Financer ／ Web designer ／ Writer</p>
+        <p class="hero__subtitle">NTU ACCT ／ Information Business Consultant ／ UI/UX Design</p>
       </div>
 
       <!-- 中欄：社群帳號 -->
